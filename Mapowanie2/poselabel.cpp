285b2c23f7339f6def4fafd48682171f74c627bd
Michal Trela <mmichal.trela@gmail.com>
2023-05-19 15:08:13 +0200

    fixed

--- Mapowanie2/poselabel.cpp
+++ Mapowanie2/poselabel.cpp
@@ -1,10 +1,14 @@
 #include "poselabel.h"
 
 PoseLabel::PoseLabel(QWidget *parent) : QWidget(parent) {
+
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
+    
+    label = new QLabel("test", this);
     label->setAlignment(Qt::AlignCenter);
+    
     layout->addWidget(label);
     setLayout(layout);
 }
